e94922026dc0e7a2bd1ec1eb496b764f1d7d73bf
Mekha1104 <gtrcooie@gmail.com>
2023-08-12 22:14:49 +0700

    06-2

--- Homework/HW06/hw06-2-roman-number.cpp
+++ Homework/HW06/hw06-2-roman-number.cpp
@@ -23,3 +23,29 @@
         929 = CMXXIX
 
 */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    
+    cout << "Enter a number: ";
+    cin >> num;
+
+    int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    string romanNumerals[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+    cout << num << " = ";
+
+    for (int i = 0; i < sizeof(values) / sizeof(values[0]); ++i) {
+        while (num >= values[i]) {
+            cout << romanNumerals[i];
+            num -= values[i];
+        }
+    }
+
+    cout << endl;
+
+    return 0;
+}
